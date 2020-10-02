1f071215acf5c634088abe6860b1bfa8ed0db2e8
parveenkhan22 <khanparveen765@gmail.com>
2020-10-03 03:39:54 +0530

    added card

--- card/src/app/app.component.ts
+++ card/src/app/app.component.ts
@@ -22,10 +22,31 @@ export class AppComponent {
      username:'biking123',
      content:' I did some biking today!'
       },
-      {title:'Mountain Biking',
-      imageUrl:'assets/biking.jpeg',
-      username:'biking123',
-      content:' I did some biking today!'
-       }
+      {title:'Mountain sunset',
+      imageUrl:'https://images.unsplash.com/photo-1476611338391-6f395a0ebc7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+      username:'hiking34',
+      content:' I did some hiking on mountain today!'
+       },
+
+       {title:'Snow ',
+       imageUrl:'https://images.unsplash.com/photo-1477430428568-b07a848ad411?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+       username:'snowfall',
+       content:' wonderlost in snow!'
+        },
+        {title:'Heaven',
+        imageUrl:'https://images.unsplash.com/photo-1470460646591-f776668990f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+        username:'Naturelove',
+        content:' somewhere lost in nature! '
+         },
+         {title:'Sunset Camping',
+         imageUrl:'https://images.unsplash.com/photo-1464207687429-7505649dae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+         username:'camping',
+         content:' This Camping is kind of Memorable!'
+          },
+          {title:'Rock Climbing',
+          imageUrl:'https://images.unsplash.com/photo-1502126324834-38f8e02d7160?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+          username:'climbing34',
+          content:' Today i did awesome climbing on Rock!'
+           }
  ]
 }
